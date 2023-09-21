f8059cd799c723b57812a534ff260ebd027c7aec
sfuentes <serg.fuentes@profesor.duoc.cl>
2023-09-21 20:25:32 -0300

    carga inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,8 +9,58 @@ const routes: Routes = [
   },
   {
     path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  }
+    loadChildren: () => import('./folder/folder.module').then(m => m.FolderPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
+  },
+  {
+    path: 'registro',
+    loadChildren: () => import('./registro/registro.module').then(m => m.RegistroPageModule)
+  },
+  {
+    path: 'cliente/listar',
+    loadChildren: () => import('./cliente/listar/cliente.listar.module').then(m => m.ClienteListarPageModule)
+  },
+  {
+    path: 'cliente/agregar',
+    loadChildren: () => import('./cliente/agregar/cliente.agregar.module').then(m => m.ClienteAgregarPageModule)
+  },
+  {
+    path: 'cliente/actualizar',
+    loadChildren: () => import('./cliente/actualizar/cliente.actualizar.module').then(m => m.ClienteActualizarPageModule)
+  },
+  {
+    path: 'cliente/eliminar',
+    loadChildren: () => import('./cliente/eliminar/cliente.eliminar.module').then(m => m.ClienteEliminarPageModule)
+  },
+
+  {
+    path: 'cliente/leer',
+    loadChildren: () => import('./cliente/leer/cliente.leer.module').then(m => m.ClienteLeerPageModule)
+  },
+  {
+    path: 'product-add',
+    loadChildren: () => import('./producto/product-add/product-add.module').then( m => m.ProductAddPageModule)
+  },
+  {
+    path: 'product-list',
+    loadChildren: () => import('./producto/product-list/product-list.module').then( m => m.ProductListPageModule)
+  },
+  {
+    path: 'product-detail/:id',
+    loadChildren: () => import('./producto/product-detail/product-detail.module').then( m => m.ProductDetailPageModule)
+  },
+  {
+    path: 'product-edit/:id',
+    loadChildren: () => import('./producto/product-edit/product-edit.module').then( m => m.ProductEditPageModule)
+  },
+  {
+    path: 'product-all',
+    loadChildren: () => import('./producto/product-all/product-all.module').then( m => m.ProductAllPageModule)
+  },
+  
 ];
 
 @NgModule({
@@ -19,4 +69,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/spam', icon: 'warning' },
+
+    { title: 'Login', url: '/login/conectar', icon: 'mail' },
+    { title: 'Registro', url: '/registro', icon: 'mail' },
+
+    { title: 'Producto/Listar', url: 'product-list', icon: 'mail' },
+    { title: 'Producto/Add', url: 'product-add', icon: 'mail' },
+    { title: 'Producto/Editar', url: 'product-edit', icon: 'mail' },
+    { title: 'Producto/Detail/1', url: 'product-detail/1', icon: 'mail' },
+    { title: 'Producto/Detail/10', url: 'product-detail/10', icon: 'mail' },
+    { title: 'Producto/Detail/', url: 'product-detail/', icon: 'mail' },
+    { title: 'Producto/Crud', url: 'product-all', icon: 'mail' },
+
+    { title: 'Cliente/Listar', url: '/cliente/listar', icon: 'mail' },
+    { title: 'Cliente/agregar', url: '/cliente/agregar', icon: 'mail' },
+    { title: 'Cliente/Actualizar', url: '/cliente/actualizar', icon: 'mail' },
+    { title: 'Cliente/Eliminar', url: '/cliente/eliminar', icon: 'mail' },
+    { title: 'Cliente/Leer', url: '/cliente/leer', icon: 'mail' },
+
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
